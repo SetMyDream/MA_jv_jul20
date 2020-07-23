d9537263c93651092e8c7eb8f6826e0e84d046a7
SetMyDream <56842438+SetMyDream@users.noreply.github.com>
2020-07-24 02:27:17 +0300

    Mates!

--- Hello-file.java
+++ Hello-file.java
@@ -1,5 +1,5 @@
 class HelloWorld {
     public static void main(String[] args) {
-        System.out.println("Hello Word! Guess what etter i`ve eaten! %nThat right! It`s '' ");
+        System.out.println("Hello Mates!");
     }
 }
